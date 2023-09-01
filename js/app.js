e3470130ccad3984df4281e81c3f6ec7bb88112d
searchsakib <searchsakib@gmail.com>
2023-09-01 22:37:20 +0600

    blog section complete

--- js/app.js
+++ js/app.js
@@ -33,6 +33,7 @@ const cardLoad = async (id) => {
 
     const cardContainer = document.getElementById('card-container');
     const div = document.createElement('div');
+
     div.innerHTML = `
     <!--card -->
     <div class="card bg-base-100 shadow-xl ">
@@ -56,7 +57,8 @@ const cardLoad = async (id) => {
                 ${item.title}
               </h2>
               <p class="text-[#171717B2] pt-[8px] pb-[9px] inline mr-2">${item.authors[0].profile_name}</p>
-              <span class='hidden'>
+              
+              <span id='span' '> 
               <svg class='inline' xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
               <g clip-path="url(#clip0_11_215)">
                 <path d="M19.375 10.0001C19.375 10.8001 18.3922 11.4595 18.1953 12.197C17.9922 12.9595 18.5063 14.022 18.1203 14.6892C17.7281 15.3673 16.5484 15.4486 15.9984 15.9986C15.4484 16.5486 15.3672 17.7282 14.6891 18.1204C14.0219 18.5064 12.9594 17.9923 12.1969 18.1954C11.4594 18.3923 10.8 19.3751 10 19.3751C9.2 19.3751 8.54062 18.3923 7.80312 18.1954C7.04062 17.9923 5.97813 18.5064 5.31094 18.1204C4.63281 17.7282 4.55156 16.5486 4.00156 15.9986C3.45156 15.4486 2.27187 15.3673 1.87969 14.6892C1.49375 14.022 2.00781 12.9595 1.80469 12.197C1.60781 11.4595 0.625 10.8001 0.625 10.0001C0.625 9.20012 1.60781 8.54075 1.80469 7.80325C2.00781 7.04075 1.49375 5.97825 1.87969 5.31106C2.27187 4.63293 3.45156 4.55168 4.00156 4.00168C4.55156 3.45168 4.63281 2.272 5.31094 1.87981C5.97813 1.49387 7.04062 2.00793 7.80312 1.80481C8.54062 1.60793 9.2 0.625122 10 0.625122C10.8 0.625122 11.4594 1.60793 12.1969 1.80481C12.9594 2.00793 14.0219 1.49387 14.6891 1.87981C15.3672 2.272 15.4484 3.45168 15.9984 4.00168C16.5484 4.55168 17.7281 4.63293 18.1203 5.31106C18.5063 5.97825 17.9922 7.04075 18.1953 7.80325C18.3922 8.54075 19.375 9.20012 19.375 10.0001Z" fill="#2568EF"/>
@@ -80,3 +82,16 @@ const cardLoad = async (id) => {
   // console.log(idData.data);
 };
 cardLoad(1000);
+
+// const span = document.getElementById('span');
+//     if (
+//       item.authors[0]?.verified !== true ||
+//       item.authors[0]?.verified === ''
+//     ) {
+//       span.classList.add('hidden');
+//     }
+
+const blogBtn = document.getElementById('blog-btn');
+blogBtn.addEventListener('click', function () {
+  window.location.href = 'blog.html';
+});
